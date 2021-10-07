4af57ae643247ca42316ba0e62b899a779085452
Camila <camachcf@dukes.jmu.edu>
2021-10-07 15:03:40 +0200

    modify func function

---
+++ class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  7 14:11:24 2021
+
+@author: camilacamacho
+"""
+
+def function (a,b):
+    return a + b 
+
+
+def function1 (a,b):
+    return a * b 
